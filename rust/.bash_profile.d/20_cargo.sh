fcc72956452f20a158bc27c69540cd8816a82255
Fynn Freyer <fynn.freyer@googlemail.com>
2025-07-28 13:36:24 +0200

    feat: tweak rust config

--- rust/.bash_profile.d/20_cargo.sh
+++ rust/.bash_profile.d/20_cargo.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# add cargo bin
+# Source cargo env if it exists
+if [[ -f "$HOME/.cargo/env" ]]; then
+  source "$HOME/.cargo/env"
+fi
+
+# Add cargo bin dir to PATH
 CARGO_BIN_DIR="$HOME/.local/opt/cargo/bin"
 if [[ -d "$CARGO_BIN_DIR" ]]; then
     export PATH="$PATH:$CARGO_BIN_DIR"

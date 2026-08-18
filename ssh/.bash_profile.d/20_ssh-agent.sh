83a1776b43a74339fd5f453d0489e668cd5c2d5f
Fynn Freyer <fynn.freyer@googlemail.com>
2026-08-18 20:44:53 +0200

    feat(ssh): improve ssh_agent script

--- ssh/.bash_profile.d/20_ssh-agent.sh
+++ ssh/.bash_profile.d/20_ssh-agent.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-shopt -s extglob
-
 if [ -z "$SSH_AUTH_SOCK" ] ; then
-  eval $(ssh-agent) > /dev/null
-  ssh-add "$HOME/.ssh"/id_!(*.pub)
+  eval "$(ssh-agent -s)" > /dev/null
+  for key in "$HOME/.ssh"/id_*; do
+    # Skip public keys
+    [[ $key == *.pub ]] && continue
+    
+    # Check if key has no password, and if so, add it
+    ssh-keygen -y -P "" -f "$key" >/dev/null 2>&1 && ssh-add "$key"
+  done
 fi
